5dc923070700b2925c093dfa82ccdd0194facc91
Oleksii Khlopotov <khlopik@gmail.com>
2018-03-26 18:40:54 +0300

    modify according to db name, added /auth/login route

--- src/app.js
+++ src/app.js
@@ -1,20 +1,40 @@
+require('../config/config.js');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const port = process.env.PORT || 8081;
+const { mongoose } = require('../db/mongoose');
+const { User } = require('../models/user');
+
+const port = process.env.PORT;
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
-	res.send({
-		title: 'Hello world!',
-		description: 'This is just a description',
-	})
+// app.get('/posts', (req, res) => {
+// 	res.send({
+// 		title: 'Hello world!',
+// 		description: 'This is just a description',
+// 	})
+// });
+
+app.post('/auth/login', (req, res) => {
+	let body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password)
+		.then(user => {
+			return user.generateAuthToken()
+				.then(token => {
+					res.header('x-auth', token).send(user);
+				});
+		})
+		.catch(e => {
+			res.status(400).send(e);
+		})
 });
 
 app.listen(port, () => {
